8dc91d7251dc455f5ca9df28e6e1f32c9a834c05
KnightNM <nimaath2004@gmail.com>
2026-09-20 16:19:56 +0530

    merge conflicts resolved in __init__.py

--- scheduler/__init__.py
+++ scheduler/__init__.py
@@ -2,7 +2,11 @@
 from scheduler.fcfs       import fcfs
 from scheduler.sjf        import sjf
 from scheduler.round_robin import round_robin
-from scheduler.priority   import priority_scheduling
+from scheduler.priority import priority_scheduling
 from scheduler.priority_rr import priority_round_robin
+from scheduler.srtf import srtf
 
-__all__ = ["fcfs", "sjf", "round_robin", "priority_scheduling", "priority_round_robin"]
+__all__ = [
+    "fcfs", "sjf", "round_robin", "priority_scheduling",
+    "priority_round_robin", "srtf",
+]
